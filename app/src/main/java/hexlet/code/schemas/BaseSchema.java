e4cf59ecab6648a114bf82d2ca35c16eab2334e0
Igor Pavlov <bujhm9987@gmail.com>
2023-05-28 23:02:16 +0300

    update build

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -4,16 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 
-public class BaseSchema {
+public abstract class BaseSchema {
 
     protected Map<String, Predicate<Object>> allChecked = new HashMap<>();
     protected boolean requiredOn = false;
 
-    protected void addCheck(String nameCheck, Predicate<Object> condition) {
+    protected final void addCheck(String nameCheck, Predicate<Object> condition) {
         allChecked.put(nameCheck, condition);
     }
 
-    public boolean isValid(Object object) {
+    public final boolean isValid(Object object) {
         if (requiredOn && object == null) {
             return false;
         }
